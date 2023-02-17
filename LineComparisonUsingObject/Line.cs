9058a74105bb688de5c4fbcac05295c1f87ee8ca
Your Name <you@example.com>
2023-02-17 14:17:26 +0530

    Checking two lines are equal or not

--- LineComparisonUsingObject/Line.cs
+++ LineComparisonUsingObject/Line.cs
@@ -16,8 +16,24 @@ namespace LineComparisonUsingObject
             Console.WriteLine("Enter the value of X2 and Y2");
             double X2 = Convert.ToDouble(Console.ReadLine());
             double Y2 = Convert.ToDouble(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow((Y2 - Y1), 2) + Math.Pow((X2 - X1), 2));
-            Console.WriteLine("Length of line is : " + length);
+            double length1 = Math.Sqrt(Math.Pow((Y2 - Y1), 2) + Math.Pow((X2 - X1), 2));
+            Console.WriteLine("Length of line 1 is : " + length1);
+            Console.WriteLine("Enter the value of X3 and Y3");
+            double X3 = Convert.ToDouble(Console.ReadLine());
+            double Y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of X4 and Y4");
+            double X4 = Convert.ToDouble(Console.ReadLine());
+            double Y4 = Convert.ToDouble(Console.ReadLine());
+            double length2 = Math.Sqrt(Math.Pow((Y4 - Y3), 2) + Math.Pow((X4 - X3), 2));
+            Console.WriteLine("Length of line 2 is : " + length2);
+            if (length2 == length1)
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equal");
+            }
         }
     }
 }
