e56b2e4a54bd32f9241145182f6d831f30ca4133
Your Name <you@example.com>
2023-02-17 14:03:58 +0530

    adding a welcome message

---
+++ LineComparisonUsingObject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineComparisonUsingObject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation program");
+
+            Console.ReadLine();
+        }
+    }
+}
